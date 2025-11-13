369dc76d7c3f5d96b34c7a3e90e8cb0ec904b222
HarolC17 <163659533+HarolC17@users.noreply.github.com>
2025-11-13 17:34:20 -0500

    Microservicio auth

---
+++ backend/auth/src/main/java/com/perfumeria/auth/application/UseCaseConfig.java
@@ -0,0 +1,17 @@
+package com.perfumeria.auth.application;
+
+import com.perfumeria.auth.domain.model.gateway.EncrypterGateway;
+import com.perfumeria.auth.domain.model.gateway.UsuarioGateway;
+import com.perfumeria.auth.domain.usecase.UsuarioUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UseCaseConfig {
+
+    @Bean
+    public UsuarioUseCase usuarioUseCase(UsuarioGateway usuarioGateway, EncrypterGateway encrypterGateway) {
+        return new UsuarioUseCase(usuarioGateway, encrypterGateway);
+    }
+
+}
